8bf6da276fa1fc6e33fcdd1de4c2d4cd38875d48
naveen <naveenannapu@hotmail.com>
2017-12-02 13:34:04 -0800

    epicodus workshop

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  alert("jQuery is working");
+})
+
+$(document).ready(function() {
+  alert($("#css-item").text());
+})
+
+$(document).ready(function() {
+  $("#goal-button").click(function() {
+    $("#goal-list").append("<li class='list-item'>" + $("#new-goal").val() + "</li>");
+    $("#new-goal").val("");
+  })
+})
